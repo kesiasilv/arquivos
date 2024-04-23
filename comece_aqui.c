b4e37c5aea802161ed8e5ed067948fad5f09eaaf
Késia Silva <kesiasilvab6@gmail.com>
2024-04-23 19:27:55 -0300

    Create comece_aqui.c

---
+++ comece_aqui.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//ultilizando fputc!!
+int main(){
+	FILE *f;//para trabalhar com arquivos, usa-se um tipo especial de ponteiro(FILE *nome_ponteiro)
+	f = fopen("arquivo.txt", "w");//abrindo um arquivo para escrita "w"
+	if (f == NULL){//caso encontrar algum erro retorna NULL, e finaliza o programa
+		printf("Erro na abertura!\n");
+		exit(1);//abortando o programa
+	}
+	char texto[20] = "Meu programa em c";//string criada para apresentar a mensagem
+	int i;
+	for(i=0; i<strlen(texto); i++){//strlen calcula o tamanho da string char(texto)
+		fputc(texto[i], f);//imprime caractere a caractere
+	}
+	fclose(f);//fecha o programa
+	return 0;
+}
