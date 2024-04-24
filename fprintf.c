0b48c3e6b5a1615271f5182d62c38d78438c0a9a
Késia Silva <kesiasilvab6@gmail.com>
2024-04-23 21:56:04 -0300

    Update fprintf.c

--- fprintf.c
+++ fprintf.c
@@ -10,8 +10,8 @@ int main(){
 		printf("Erro na abertura!");
 		exit(1);
 	}
-	printf("Nome: %s \nIdade: %d\nAltura: %.2f\n", nome, i, a);
-	fprintf(arq,"Nome: %s\nIdade: %d\nAltura: %.2f\n", nome, i, a);
+	printf("Nome: %s \nIdade: %d\nAltura: %.2f\n", nome, i, a);//imprime no prompt de comando
+	fprintf(arq,"Nome: %s\nIdade: %d\nAltura: %.2f\n", nome, i, a);//imprime no bloco de notas
 	fclose(arq);
 	
 	return 0;
