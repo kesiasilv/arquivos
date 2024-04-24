f2cc903ea1c217120e7b321d97b7d213c4a6402d
Késia Silva <kesiasilvab6@gmail.com>
2024-04-23 21:55:16 -0300

    Create fprintf.c

---
+++ fprintf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char nome[20] = "kesia";
+	int i = 19;
+	float a = 1.52;
+	FILE *arq = fopen("arquivo.txt", "w");
+	if(arq == NULL){
+		printf("Erro na abertura!");
+		exit(1);
+	}
+	printf("Nome: %s \nIdade: %d\nAltura: %.2f\n", nome, i, a);
+	fprintf(arq,"Nome: %s\nIdade: %d\nAltura: %.2f\n", nome, i, a);
+	fclose(arq);
+	
+	return 0;
+}
